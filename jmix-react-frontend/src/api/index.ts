db21ed6caca1367380162ca5eb91de167dac02ab
kl3inIT <dathip04@gmail.com>
2025-12-31 14:41:02 +0700

    ok

--- jmix-react-frontend/src/api/index.ts
+++
@@ -1,4 +0,0 @@
-export * from "./hooks";
-export * from "./types";
-export { QueryProvider } from "./QueryProvider";
-export { HttpError } from "./HttpError";
--- jmix-react-frontend/src/components/index.ts
+++
@@ -1,4 +0,0 @@
-export * from "./error";
-export * from "./layout";
-
-
